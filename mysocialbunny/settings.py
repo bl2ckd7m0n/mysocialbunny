765c25f298f528f6c6798c3342d77657e8252fd5
Freddy Alexander <freddy_alexander@outlook.com>
2019-09-26 06:54:09 -0500

    Update settings.py v1.0

--- mysocialbunny/settings.py
+++ mysocialbunny/settings.py
@@ -150,9 +150,15 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
-STATICFILES_DIRS = (
+
+# Deployment
+# STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+
+# Localhost
+STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static'),
-)
+]
+
 STATICFILES_FINDERS = [
     'django.contrib.staticfiles.finders.FileSystemFinder',
     'django.contrib.staticfiles.finders.AppDirectoriesFinder',
